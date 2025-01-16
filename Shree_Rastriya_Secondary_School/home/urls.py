b4d32f38b984d1ebe37707262b481626b665de96
Shishir-kc <shishir.khatri.kc@gmail.com>
2025-01-16 10:45:52 +0545

    starting to add admin panel

--- Shree_Rastriya_Secondary_School/home/urls.py
+++ Shree_Rastriya_Secondary_School/home/urls.py
@@ -7,5 +7,5 @@ app_name="home"
 urlpatterns = [  
     path('', views.home, name='home'),
     path('login/',views.user_login,name="login"),
-    path('aboutus/',views.about_us,name="about_us")
+    path('aboutus/',views.about_us,name="about_us"),
 ]
