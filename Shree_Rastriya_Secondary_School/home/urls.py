b3ccd3c9f2965d2826f3b57f8d27dfe8c0e649b6
Shishir-kc <shishir.khatri.kc@gmail.com>
2025-01-16 11:21:39 +0545

    updated urls for admin

--- Shree_Rastriya_Secondary_School/home/urls.py
+++ Shree_Rastriya_Secondary_School/home/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/',views.user_login,name="login"),
     path('aboutus/',views.about_us,name="about_us"),
+    path('Shree-Rastriya-Secondary-School/admin',views.admin_panel,name="admin-settings"),
+    path('Shree-Rastriya-Secondary-School/admin/event/lists',views.Events_List,name='events'),
 ]
