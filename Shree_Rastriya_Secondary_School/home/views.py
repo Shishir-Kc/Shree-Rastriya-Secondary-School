782658197c8d5d71bf6dd7cbedef4ddd49474490
Shishir-kc <shishir.khatri.kc@gmail.com>
2025-01-16 10:44:46 +0545

    added login function

--- Shree_Rastriya_Secondary_School/home/views.py
+++ Shree_Rastriya_Secondary_School/home/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from . import models
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required , permission_required
 
 
 
@@ -12,6 +14,14 @@ def home(request):
     return render(request,'home/home.html',context)
 
 def user_login(request):
+    if request.method == "POST":
+        # print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+        user_name = request.POST.get('user_name')
+        user_pass = request.POST.get('user_pass')
+        user = authenticate(request,username=user_name,password=user_pass)
+        if user is not None:
+            login(request,user)
+            return redirect('home:home')
     return render(request,'home/login.html')
 
 def about_us(request):
